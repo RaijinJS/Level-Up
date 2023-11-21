664f943aa501fda52c42ac846aa88100b2aa27ea
Niels Koop <business.klnkoop@gmail.com>
2023-11-21 17:32:52 +0100

    refactor: implemented redux for tasks state

--- level-up-2/app/layout.tsx
+++ level-up-2/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { ReduxProvider } from "../redux/provider";
 // TODO: add redux here
 
 const nunito = Nunito({
@@ -16,17 +17,19 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${nunito.className} bg-gradient-to-r from-orange-100 via-orange-200 to-orange-300`}>
-        {/* TODO: Remove nav here completely and use css to center the div below */}
-        <nav>
-          <div className="display:none">
-            {/* TODO: Replace favicon with the logo */}
-            {/* Logo */}
-            <img src="/logo1.png" alt="Level Up Logo" className="h-20 md:h-28 opacity-0" />
-          </div>
-        </nav>
-        <main className="container mx-auto px-4 mt-8">{children}</main>
-      </body>
+      <ReduxProvider>
+        <body className={`${nunito.className} bg-gradient-to-r from-orange-100 via-orange-200 to-orange-300`}>
+          {/* TODO: Remove nav here completely and use css to center the div below */}
+          <nav>
+            <div className="display:none">
+              {/* TODO: Replace favicon with the logo */}
+              {/* Logo */}
+              <img src="/logo1.png" alt="Level Up Logo" className="h-20 md:h-28 opacity-0" />
+            </div>
+          </nav>
+          <main className="container mx-auto px-4 mt-8">{children}</main>
+        </body>
+      </ReduxProvider>
     </html>
   );
 }
