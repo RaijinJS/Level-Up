8186494fc3816fabc1c9a685372f847fc7193cbf
raijin <christopherheinzmann@outlook.com>
2023-11-22 21:29:11 +0100

    re-implement randomized cards

--- level-up-2/app/api/tasks/route.ts
+++ level-up-2/app/api/tasks/route.ts
@@ -8,9 +8,15 @@ export async function POST(request: Request) {
     await connectMongoDB();
     const res = await Task.create({ title, description, image });
     if (res.ok) {
-      return NextResponse.json({ message: "Task generated successfully" }, { status: 201 });
+      return NextResponse.json(
+        { message: "Task generated successfully" },
+        { status: 201 }
+      );
     } else {
-      return NextResponse.json({ message: "Task could not be created" }, { status: 500 });
+      return NextResponse.json(
+        { message: "Task could not be created" },
+        { status: 500 }
+      );
     }
   } catch (error) {
     console.log(error);
@@ -21,28 +27,22 @@ export async function POST(request: Request) {
 export async function PUT(request: Request) {
   try {
     await connectMongoDB();
-
-    // Find a random task with added set to false
     const randomTask = await Task.aggregate([
       { $match: { added: false } },
       { $sample: { size: 1 } },
     ]);
-
     if (!randomTask || randomTask.length === 0) {
-      console.log('none left')
+      console.log("none left");
       return NextResponse.json(
         { message: "No unadded tasks remaining" },
         { status: 404 }
       );
     }
-
-    // Update the selected task's added field to true
     const updatedTask = await Task.findOneAndUpdate(
       { _id: randomTask[0]._id },
       { $set: { added: true } },
       { new: true }
     );
-
     return NextResponse.json(updatedTask);
   } catch (error) {
     console.log(error);
