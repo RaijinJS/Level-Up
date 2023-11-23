1496d610fa92523e45e2d528cff1b3f7dd5d4e64
Niels Koop <business.klnkoop@gmail.com>
2023-11-23 11:32:46 +0100

    fix: fixed eslint error in test-utils

--- level-up-2/utils/tests/test.utils.tsx
+++ level-up-2/utils/tests/test.utils.tsx
@@ -19,7 +19,7 @@ export function renderWithProviders(
     ...renderOptions
   }: ExtendedRenderOptions = {},
 ) {
-  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+  function Wrapper({ children }: PropsWithChildren<NonNullable<unknown>>): JSX.Element {
     return <Provider store={store}>{children}</Provider>;
   }
 
